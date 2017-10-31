dd5b212fdc857217a4cf59e2a8f609e51053ce24
yuvalheftman@gmail.com <yuvalheftman@gmail.com>
2017-10-31 20:11:34 +0200

    fix

--- src/app/trade/trade.component.ts
+++ src/app/trade/trade.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import {NgRedux, select, select$} from '@angular-redux/store';
+import { Component } from '@angular/core';
+import {NgRedux, select$} from '@angular-redux/store';
 import {AppState} from '../app.redux';
 import {Observable} from 'rxjs/Observable';
 import {OutstandingOrder, Transaction} from '../app.model';
 import {Router} from '@angular/router';
 
-const top20OutstandingOrdersAsc = data => data.map( q => q.filter(item => item.outstanding > 0)).map( x => x.slice(0, 20));
-const top20OutstandingOrdersDesc = data => data.map( q => q.filter(item => item.outstanding > 0)).map( x => x.slice(0, 20));
-
-const last30 = data => data.map(a => a.sort((x , y) => y.timeStamp - x.timeStamp )).map( c => c.slice(0, 30));
-
+export const top20OutstandingOrders = data => data.map( q => q.filter(item => item.outstanding > 0)).map( x => x.slice(0, 20));
+export const last30 = data => data.map(a => a.sort((x , y) => y.timeStamp - x.timeStamp )).map( c => c.slice(0, 30));
+export const length = obs$ => obs$.map( item => item.length);
 @Component({
   selector: 'app-trade',
   templateUrl: './trade.component.html',
@@ -26,19 +24,19 @@ export class TradeComponent {
     {name: 'Sale', prop: 'saleId'},
     { name: 'Quantity' },
     { name: 'Price' }
-  ]
-  @select$(['askQueue'], top20OutstandingOrdersAsc )
+  ];
+  @select$(['askQueue'], top20OutstandingOrders )
   sellOrders: Observable<OutstandingOrder>;
-  @select$(['bidQueue'], top20OutstandingOrdersDesc)
+  @select$(['bidQueue'], top20OutstandingOrders)
   buyOrders: Observable<OutstandingOrder>;
   @select$(['transactions'], last30)
   transactions: Observable<Transaction>;
 
-  @select$(['transactions'], obs$ => obs$.map( item => item.length))
+  @select$(['transactions'], length)
   transNum: Observable<Number>;
-  @select$(['bidQueue'], obs$ => obs$.map( item => item.length))
+  @select$(['bidQueue'], length)
   buyNum: Observable<Number>;
-  @select$(['askQueue'], obs$ => obs$.map( item => item.length))
+  @select$(['askQueue'], length)
   sellNum: Observable<Number>;
   constructor(private ngRedux: NgRedux<AppState>, private router: Router) { }
   transctionSelect(e: any) {
