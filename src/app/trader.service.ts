bdec9324f27ce21816158ff4546227312cf30f35
yuvalheftman@gmail.com <yuvalheftman@gmail.com>
2017-11-01 19:38:27 +0200

    rewrite trading logic

--- src/app/trader.service.ts
+++ src/app/trader.service.ts
@@ -24,50 +24,37 @@ export class OrderService {
     const transactions: Transaction[] = this.performTransactions(sellOrders, buyOrders);
     this.ngRedux.dispatch({type: APP_TRADING_CYCLE_END, payload: transactions});
   }
+
   performTransactions(sell: OutstandingOrder[] , buy: OutstandingOrder[]): Transaction[] {
     const transactions: Transaction[] = [];
     // filter outstanding ,sort price DESC
-    buy = buy.map( item => item).filter(item => item.outstanding > 0).sort((a, b ) => (b.price - a.price) * 10 + (a.id > b.id ? 1 : -1));
+    buy = buy.map(item => item).filter(item => item.outstanding > 0).sort((a, b) => (b.price - a.price) * 10 + (a.id > b.id ? 1 : -1));
     // filter outstanding ,sort price ASC
-    sell = sell.map( item => item).filter(item => item.outstanding > 0).sort((a , b) => (a.price - b.price) * 10 + (a.id > b.id ? 1 : -1));
+    sell = sell.map(item => item).filter(item => item.outstanding > 0).sort((a, b) => (a.price - b.price) * 10 + (a.id > b.id ? 1 : -1));
 
     const sellNum = sell.length;
     const buyNum = buy.length;
-    // the buy index is defined here to keep track of the last buy order while changing sell orders.
-    let buyIndex = 0;
-    sellLoop:
-      for (let sellIndex = 0; sellIndex < sellNum; sellIndex++) {
-        const sellOrder = sell[sellIndex];
-          for (; buyIndex < buyNum; buyIndex++) {
-            const buyOrder = buy[buyIndex];
-            // no more possible transactions
-            if (buyOrder.price < sellOrder.price) {
-              return transactions;
-            }
-            if (buyOrder.outstanding === 0) {
-              if ( buyIndex + 1 === buyNum) {
-                // last buy outstanding = 0
-                return transactions;
-              }
-              // go to next buy order
-              continue;
-            }
-            const amount  = Math.min(buyOrder.outstanding , sellOrder.outstanding);
 
-            // create transaction
-            transactions.push({id: sellOrder.id + '-' + buyOrder.id, saleId: sellOrder.id, buyId: buyOrder.id,
-                  quantity: amount, price: (buyOrder.price + sellOrder.price) / 2
-                  , timeStamp: (new Date()).getTime()});
-            // update orders
-            sellOrder.outstanding -= amount;
-            buyOrder.outstanding -= amount;
-            // This is the case the last sell completed, but last buy not. go to next sell order
-            if (sellOrder.outstanding === 0) {
-              continue sellLoop;
-            }
-          }
+    let sellIndex = 0, buyIndex = 0;
+    while (sellIndex < sellNum && buyIndex < buyNum && buy[buyIndex].price >= sell[sellIndex].price) {
+      const sellOrder = sell[sellIndex];
+      const buyOrder = buy[buyIndex];
+      const amount  = Math.min(buyOrder.outstanding , sellOrder.outstanding);
+
+      // create transaction
+      transactions.push({id: sellOrder.id + '-' + buyOrder.id, saleId: sellOrder.id, buyId: buyOrder.id,
+        quantity: amount, price: (buyOrder.price + sellOrder.price) / 2
+        , timeStamp: (new Date()).getTime()});
+      // update orders
+      sellOrder.outstanding -= amount;
+      buyOrder.outstanding -= amount;
+      if (sellOrder.outstanding === 0) {
+        sellIndex++;
       }
-
+      if (buyOrder.outstanding === 0) {
+        buyIndex++;
+      }
+    }
     return transactions;
   }
   getOrders(start: number) {
